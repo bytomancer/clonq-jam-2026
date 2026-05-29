815bac9cb8c17c76dd951ded3b52e25f7dd79bfa
Bytomancer <bytomancer@gmail.com>
2026-05-29 18:52:42 -0400

    Got statuses communicating to harness

---
+++ godot/src/lose.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export
+var timer = 2.0
+
+func _ready() -> void:
+	JavaScriptBridge.eval("window.parent.postMessage({op: \"done\", win: false});")
+
+func _process(delta: float) -> void:
+	timer -= delta
+	if timer <= 0.0:
+		get_tree().change_scene_to_file("res://scn/pre.tscn")
--- godot/src/pre.gd
+++ godot/src/pre.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+func _ready() -> void:
+	JavaScriptBridge.eval("window.parent.postMessage({op: \"ready\"});")
+
 func _process(_delta: float) -> void:
 	var diff = JavaScriptBridge.eval("window.lcolonqJamStart || -1.0")
 	diff = diff as int if diff else -1
 	if diff > -0.5:
 		JavaScriptBridge.eval("window.lcolonqJamStart = -1.0")
+		JavaScriptBridge.eval("window.parent.postMessage({op: \"started\"});")
 		var main_scene = load("res://scn/main.tscn")
 		var scn = main_scene.instantiate()
 		scn.set_difficulty(diff)
