843375f2e7f968de17b5637c3b6abe5f90c99595
Bytomancer <bytomancer@gmail.com>
2026-07-14 19:47:41 -0400

    fix: adjusted rendering for release

--- godot/src/pre.gd
+++ godot/src/pre.gd
@@ -6,6 +6,8 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	var diff = JavaScriptBridge.eval("window.lcolonqJamStart || -1.0")
 	diff = diff as int if diff else -1
+	if diff < -0.5 && Input.is_action_just_released("ui_accept"):
+		diff = 0.0
 	if diff > -0.5:
 		JavaScriptBridge.eval("window.lcolonqJamStart = -1.0")
 		JavaScriptBridge.eval("window.parent.postMessage({op: \"started\"});")
